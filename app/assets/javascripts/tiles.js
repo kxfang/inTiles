47400b65da35276f65f4eb3b193b54ab6d1969a6
Kevin Fang <kfang@kfang.com>
2012-10-30 23:59:25 -0700

    checkpoint: added javascript for generating tiles

--- app/assets/javascripts/tiles.js
+++ app/assets/javascripts/tiles.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
 
+  var rows = 5;
+  var cols = 5;
+
   $.getJSON("/api/li-connections", function(json) {
     $(".content").html("connections loaded");
-    console.log(json);
+    generateTiles();
   });
+  
+  function generateTiles() {
+    $('.tile-box').each(function(index, tilesDiv) {
+      console.log('generating tiles for index: ' + index);
+      for (var r = 0; r < rows; r++) {
+        var row = $('' + $('#rowTpl').html()).appendTo(tilesDiv);
+        console.log($('#rowTpl').html());
+        for (var c = 0; c < cols; c++) {
+          row.append($('#textTileTpl').html());
+        }
+      }
+    });
+  }
 });
+
+
 
--- app/controllers/tiles_controller.rb
+++ app/controllers/tiles_controller.rb
@@ -1,4 +1,6 @@
 class TilesController < ApplicationController
   def game
+    @rows = 5
+    @cols = 5
   end
 end
